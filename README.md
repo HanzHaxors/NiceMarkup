6ff3662d4a05c61521b6d14f1ab3441ee7f524b8
Hanz <40712686+HanzCEO@users.noreply.github.com>
2021-11-08 08:34:09 +0700

    Add HTML Attribute Support and README (#1)
    * Add support for HTML attributes and a new example
    * Add README.md
    * Add `npm-publish.yml`
    * Bump version to v0.2.0

---
+++ README.md
@@ -0,0 +1,21 @@
+# NiceMarkup
+NiceMarkup is a simple language to help web developers build their website. As simple as it is, it has no configuration file, ever.
+
+## Hello World
+```nice
+html
+	head
+		title
+			text: NiceMarkup Hello World
+	body
+		h1
+			text: Hello World!
+```
+> Taken from [examples/hello.nice](examples/hello.nice)
+
+We don't have any closing tag and we use css selector to define the element
+
+## Simple Compiler
+```sh
+node index.js <sourceFile> [destinationFile]
+```
--- walker/node.js
+++ walker/node.js
@@ -1,3 +1,12 @@
+function assembleAttributes(attrs) {
+	let result = " ";
+	for (const attr in attrs) {
+		result += `${attr}="${attrs[attr]}" `;
+	}
+
+	return result.substring(0, result.length-1);
+}
+
 class Node {
 	constructor(tag) {
 		this.tag = tag;
@@ -18,7 +27,7 @@ class Node {
 
 		if (this.tag == "text") return `${tabs}${this.attributes.value}\n`;
 
-		let result = `${tabs}<${this.tag}>\n`;
+		let result = `${tabs}<${this.tag}${assembleAttributes(this.attributes)}>\n`;
 
 		for (const child of this.children) {
 			result += child.toString(tabAmount + 1);
