8a9b9863b76af2cd7443385de8845600381e7d32
Hanz <40712686+HanzCEO@users.noreply.github.com>
2021-11-10 13:01:32 +0700

    Add Multi-file Support (#4)
    * Added multi-file support
    * Bump file version to v0.2.0-rc4

--- utils/index.js
+++ utils/index.js
@@ -1,10 +1,3 @@
-function getFilename(path) {
-	let parts = path.split('/');
-	return parts[parts.length-1];
-}
-
 module.exports = {
-	getFilename,
 
-	print: console.log
 };
--- walker/index.js
+++ walker/index.js
@@ -1,5 +1,5 @@
-const { print } = require('../utils');
 const Node = require('./node');
+const preprocessors = require('./preprocessors');
 
 function deletePreTabs(line) {
 	return line.substring(getTabIndex(line));
@@ -20,11 +20,22 @@ function isElement(line) {
 }
 
 function isProperty(line) {
-	line = deletePreTabs(line);
+	line = deletePreTabs(line).trim();
 	const alphregex = /[a-zA-Z]/;
 	const regex = /[a-zA-Z0-9]+:.*/;
 
 	return line.length && alphregex.test(line[0]) && regex.test(line);
+}
+
+function isGroupMember(line) {
+	line = deletePreTabs(line);
+	return line.startsWith('-');
+}
+
+function isPreProcessor(line) {
+	let regex = /#[a-zA-Z]+/;
+	let action = line.substring(1).split(' ')[0];
+	return regex.test(line) && Object.keys(preprocessors).includes(action);
 }
 
 function getElementTag(line) {
@@ -37,7 +48,7 @@ function getElementTag(line) {
 
 function getProperty(line) {
 	let name = "", value = "";
-	line = deletePreTabs(line);
+	line = deletePreTabs(line).trim();
 	line = line.split(':');
 
 	name = line[0];
@@ -50,20 +61,52 @@ function getProperty(line) {
 function process(source) {
 	/* Position variable */
 	let col = 0, row = 0, lastTab = 0, tab = 0;
+	let groupMemberIndex = 0;
 	let lastTagIndex = 0;
+	let lastTagName = "";
 
 	function tabs() { return '\t'.repeat(tab); }
 
 	/* Creating the tree */
-	const root = new Node('root');
-	let parentNode = root;
+	let files = [];
+	let parentNode = undefined;
 	let lines = source.split('\n');
 
-	for (const line of lines) {
+	for (let line of lines) {
 		/* Skips comment lines */
-		if (line.replaceAll('\t', '').startsWith('#')) continue;
+		/* But first, lets check if it is pre processor */
+		if (!isPreProcessor(line) && line.replaceAll('\t', '').startsWith('#')) continue;
+		if (line.length == 0) continue;
+
+		/* Process pre-processor */
+		if (isPreProcessor(line)) {
+			const arguments = line.substring(1).split(' ');
+			const action = arguments[0];
+			arguments.shift();
+
+			const response = preprocessors[action](arguments);
+
+			switch (action) {
+				case "file":
+					files.push(response);
+					parentNode = undefined;
+					break;
+			}
+
+			continue;
+		}
 
 		tab = getTabIndex(line);
+
+		/* Are you HTML? */
+		if (files.length == 0) {
+			throw "Please specify file name first before writing content";
+		} if (files[files.length-1].type != "html") {
+			files[files.length-1].append(line + '\n');
+			continue;
+		} else if (parentNode == undefined) {
+			parentNode = files[files.length-1].content;
+		}
 
 		/* Get element tag */
 		if (isElement(line)) {
@@ -77,11 +120,26 @@ function process(source) {
 			} else if (lastTab == tab) {
 				node = parentNode;
 			} else {
-				print("Error");
+				console.log("Error");
 			}
 
 			let newNode = new Node(tag);
 			lastTagIndex = node.addChild(newNode) - 1;
+			lastTag = tag;
+			groupMemberIndex = 0;
+		}
+
+		/* Process group member */
+		if (isGroupMember(line)) {
+			if (groupMemberIndex == 0) {
+				/* Remove the group tag name identifier node */
+				parentNode.children = parentNode.children.slice(0, lastTagIndex);
+			}
+
+			line = line.replace(/\-\s*/, '');
+			let clone = new Node(lastTag);
+			lastTagIndex = parentNode.addChild(clone) - 1;
+			groupMemberIndex++;
 		}
 
 		/* Get Property */
@@ -102,8 +160,10 @@ function process(source) {
 		lastTab = tab;
 	}
 
-	/* Makin the result */
-	return root.children[0].toString();
+	/* Making the result */
+	return {
+		files
+	};
 }
 
 module.exports = {
